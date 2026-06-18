7b426018b4523812d5a0e08273e7329d46b76c1a
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2026-06-18 16:22:52 +0700

    Optimize tqdm for background running (nohup)

--- fetch_limuc.py
+++ fetch_limuc.py
@@ -12,7 +12,7 @@ def download_file(url, dest_path):
     total_size_in_bytes = int(response.headers.get('content-length', 0))
     block_size = 1024 # 1 Kibibyte
     
-    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc=os.path.basename(dest_path))
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, desc=os.path.basename(dest_path), mininterval=10.0)
     with open(dest_path, 'wb') as file:
         for data in response.iter_content(block_size):
             progress_bar.update(len(data))
